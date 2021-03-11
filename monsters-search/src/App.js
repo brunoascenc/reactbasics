c946522de77c6eb81b70b7be7f5efe49321a1cd9
brunoascenc <brunoascenc1@outlook.com>
2021-03-11 11:54:35 -0300

    arrow functions

--- monsters-search/src/App.js
+++ monsters-search/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import "./App.css";
 import { CardList } from "./components/card-list/CardList";
+import { SearchBox } from "./components/search-box/SearchBox";
 
 class App extends Component {
   constructor() {
@@ -10,6 +11,8 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    // this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -17,6 +20,10 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
     const { monsters, searchField } = this.state;
@@ -27,10 +34,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
           placeholder="search monster"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
